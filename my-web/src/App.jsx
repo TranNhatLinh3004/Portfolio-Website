8913170ae45c0977279cac6d0f17da906d1205d9
Trần Nhật Linh <linhtran.it.3004@gmail.com>
2024-07-25 10:59:58 +0700

    Init web

---
+++ my-web/src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.scss";
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Features</section>
+      <section>Testimonials</section>
+      <section>CTA</section>
+      <section>Footer</section>
+    </div>
+  );
+};
+
+export default App;
